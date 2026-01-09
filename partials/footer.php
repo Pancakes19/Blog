b6540eb66ae29cfbfdff79efb1866e65e1514656
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-09 15:03:03 +0200

    using the header and footer files to add those sections to all other files

---
+++ partials/footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer__socials">
+    <a href="insta" target="_blank"><i class="uil uil-instagram"></i></a>
+    <a href="https://www.linkedin.com/in/quinton-khuwiseb-b85b432a2/" target="_blank"><i class="uil uil-linkedin-alt"></i></a>
+    <a href="https://github.com/Pancakes19" target="_blank"><i class="uil uil-github-alt"></i></a>
+    <a href="mailto:khuwisebquinton@gmail.com"><i class="uil uil-envelope"></i></a>
+  </div>
+  <div class="container footer__container">
+  <div class="footer__header"><h4>Links</h4></div>
+  <div class="footer__links">
+    <article>
+      <ul>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Linkedin</a></li>
+      </ul>
+    </article>
+    <article>
+      <ul>
+        <li><a href="">Github</a></li>
+        <li><a href="">My Portfolio</a></li>
+      </ul>
+    </article>
+    <article>
+      <ul>
+        <li><a href="">Email</a></li>
+        <li><a href="">WhatsApp me</a></li>
+      </ul>
+    </article>
+  </div>
+</div>
+
+  <div class="footer__copyright">
+    <small>Copyright &copy; QUINTON KHUWISEB</small>
+  </div>
+
+
+</footer>
+
+
+<script src="./JS/main.js"></script>
+</body>
+</html>
