7291e115d44c95201c1a4a355f493000ab298fa6
Quinton Khuwiseb <khuwisebq@gmail.com>
2026-01-15 18:21:47 +0200

    divide users access to admin access

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -13,41 +13,33 @@ include 'partials/header.php';
                 <h5>Add Post</h5>
               </a>
             </li>
-          </ul>
-          <ul>
             <li>
               <a href="index.php"><i class="uil uil-postcard"></i>
                 <h5>Manage Posts</h5>
               </a>
             </li>
-          </ul>
-          <ul>
-            <li>
+			<?php if(isset($_SESSION['user_is_admin'])) : ?>
+           <li>
               <a href="add-user.php"><i class="uil uil-user-plus"></i>
                 <h5>Add User</h5>
               </a>
             </li>
-          </ul>
-          <ul>
             <li>
               <a href="manage-users.php"><i class="uil uil-users-alt"></i>
                 <h5>Manage Users</h5>
               </a>
             </li>
-          </ul>
-          <ul>
             <li>
               <a href="add-category.php"><i class="uil uil-edit"></i>
                 <h5>Add Category</h5>
               </a>
             </li>
-          </ul>
-          <ul>
             <li>
               <a href="manage-categories.php" class="active"><i class="uil uil-list-ul"></i>
                 <h5>Manage Categories</h5>
               </a>
             </li>
+			<?php endif ?>
           </ul>
         </aside>
         <main>
--- admin/manage-users.php
+++ admin/manage-users.php
@@ -18,6 +18,7 @@ include 'partials/header.php';
                 <h5>Manage Posts</h5>
               </a>
             </li>
+			<?php if(isset($_SESSION['user_is_admin'])) : ?>
             <li>
               <a href="add-user.php"><i class="uil uil-user-plus"></i>
                 <h5>Add User</h5>
@@ -38,6 +39,7 @@ include 'partials/header.php';
                 <h5>Manage Categories</h5>
               </a>
             </li>
+			<?php endif ?>
           </ul>
         </aside>
         <main>
